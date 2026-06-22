f49d15d5e31638bdfa4288f64c491966de6edfd3
要恰饭的嘛 <1243411723@qq.com>
2026-06-22 09:30:59 +0800

    项目创建

---
+++ AutomaticWeighingSystem/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using AutomaticWeighingSystem.Views;
+using Prism.Ioc;
+
+namespace AutomaticWeighingSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
